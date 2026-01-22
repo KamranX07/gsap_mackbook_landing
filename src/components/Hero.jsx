778eb27be6e7d09eb51891ed6c08c1cba9c5543b
Md Kamran Akhter <mdkamranakhter78@gmail.com>
2026-01-23 00:00:20 +0530

    fixed typo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <section id="hero">
             <div>
                 <h1>MacBook Pro</h1>
-                <img src="/title.png" alt="Mackbook Title" />
+                <img src="/title.png" alt="Macbook Title" />
             </div>
 
             <video ref={videoRef} src="/videos/hero.mp4" autoPlay muted playsInline />
